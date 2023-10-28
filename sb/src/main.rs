9dc4a201698e174c53b12ded7688bffcf3f47c5c
neruneruna7 <113534341+neruneruna7@users.noreply.github.com>
2023-10-28 17:58:27 +0900

    add: Httpリクエストを投げてみた

--- sb/src/main.rs
+++ sb/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let response = reqwest::get("https://www.google.com").await?;
+    println!("Status: {}", response.status());
+    println!("Headers:\n{:?}", response.headers());
+    println!("Body:\n{}", response.text().await?);
+    Ok(())
 }
