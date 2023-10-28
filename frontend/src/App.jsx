467f3c7dec55bfcaa92be26714b6562e460a105b
haruto0707 <221205070@ccmailg.meijo-u.ac.jp>
2023-10-28 16:20:53 +0900

    add: boxの配置

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,46 @@ function App() {
   return (
     <div className="desktop">
       <div className="frame">
+        <div className="box">
+          <button className="rectangle-button">
+            <div className="rectangle"></div>
+          </button>
+        </div>
+        <div className="box">
+          <button className="rectangle-button">
+            <div className="rectangle"></div>
+          </button>
+        </div>
+        <div className="box">
+          <button className="rectangle-button">
+            <div className="rectangle"></div>
+          </button>
+        </div>
+        <div className="box">
+          <button className="rectangle-button">
+            <div className="rectangle"></div>
+          </button>
+        </div>
+        <div className="box">
+          <button className="rectangle-button">
+            <div className="rectangle"></div>
+          </button>
+        </div>
+        <div className="box">
+          <button className="rectangle-button">
+            <div className="rectangle"></div>
+          </button>
+        </div>
+        <div className="box">
+          <button className="rectangle-button">
+            <div className="rectangle"></div>
+          </button>
+        </div>
+        <div className="box">
+          <button className="rectangle-button">
+            <div className="rectangle"></div>
+          </button>
+        </div>
       </div>
       <div className="title">
         <div className="model-s">パンダに見えるのはどれ？</div>
@@ -13,12 +53,6 @@ function App() {
       <button className="input-field-button">
         <div className="input-field">
           <div className="text-wrapper">VERIFY</div>
-        </div>
-      </button>
-
-      <button className="box-button">
-        <div className="box">
-          <div className="rectangle" />
         </div>
       </button>
 
