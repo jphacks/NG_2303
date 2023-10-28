a82544ccd0a86ba6b1672097ae08fc1241b32a87
haruto0707 <221205070@ccmailg.meijo-u.ac.jp>
2023-10-28 22:07:51 +0900

    add: チェックマーク表示非表示機能の追加

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,62 +1,34 @@
+import React, { useState } from 'react';
 import './App.css';
 import "./style.css";
 import icon from './icon-check.png';
 
 function App() {
+  const [buttonStates, setButtonStates] = useState(
+    Array(8).fill(false)  // 8つのボタンに対する状態を保持する配列。初期状態は全て非表示（false）。
+  );
+
+  const toggleIcon = index => {
+    const newButtonStates = [...buttonStates];
+    newButtonStates[index] = !newButtonStates[index];  // 状態を反転させる
+    setButtonStates(newButtonStates);
+  };
+
   return (
     <div className="desktop">
       <div className="frame">
-        <div className="box">
-          <button className="rectangle-button">
-            <div className="rectangle">
-            <img className="icon-check" alt="Icon check" src={icon} /></div>
-          </button>
-        </div>
-        <div className="box">
-          <button className="rectangle-button">
-            <div className="rectangle">
-            <img className="icon-check" alt="Icon check" src={icon} /></div>
-          </button>
-        </div>
-        <div className="box">
-          <button className="rectangle-button">
-            <div className="rectangle">
-            <img className="icon-check" alt="Icon check" src={icon} /></div>
-          </button>
-        </div>
-        <div className="box">
-          <button className="rectangle-button">
-            <div className="rectangle">
-            <img className="icon-check" alt="Icon check" src={icon} /></div>
-          </button>
-        </div>
-        <div className="box">
-          <button className="rectangle-button">
-            <div className="rectangle">
-            <img className="icon-check" alt="Icon check" src={icon} /></div>
-          </button>
-        </div>
-        <div className="box">
-          <button className="rectangle-button">
-            <div className="rectangle">
-            <img className="icon-check" alt="Icon check" src={icon} /></div>
-          </button>
-        </div>
-        <div className="box">
-          <button className="rectangle-button">
-            <div className="rectangle">
-            <img className="icon-check" alt="Icon check" src={icon} /></div>
-          </button>
-        </div>
-        <div className="box">
-          <button className="rectangle-button">
-            <div className="rectangle">
-            <img className="icon-check" alt="Icon check" src={icon} /></div>
-          </button>
-        </div>
+        {buttonStates.map((isVisible, index) => (
+          <div className="box" key={index}>
+            <button className="rectangle-button" onClick={() => toggleIcon(index)}>
+              <div className="rectangle">
+                {isVisible && <img className="icon-check" alt="Icon check" src={icon} />}
+              </div>
+            </button>
+          </div>
+        ))}
       </div>
-      {/* <div className="frame1"> */}
-        {/* </div> */}
+      {/* <div className="frame1"> */ }
+  {/* </div> */ }
       <div className="title">
         <div className="model-s">パンダに見えるのはどれ？</div>
       </div>
@@ -66,7 +38,6 @@ function App() {
           <div className="text-wrapper">VERIFY</div>
         </div>
       </button>
-
     </div>
   );
 }
