a0a5d0ec7fd690fa25ec0d1310407b5524e9c876
rikuto seike <221205091@ccmailg.meijo-u.ac.jp>
2023-10-29 09:48:42 +0900

    フロント完成

--- frontend/src/Result.jsx
+++ frontend/src/Result.jsx
@@ -10,38 +10,40 @@ function Result() {
   const [buttonStates, setButtonStates] = useState(location.state.buttonStates || Array(8).fill(false));
   const navigate = useNavigate();
 
+  // 正しい答えを示す配列。これは仮のもので、実際にはこの値を設定する必要があります。
+  const correctAnswers = [true, false, true, false, true, false, true, false];
+
   useEffect(() => {
     if (location.state && location.state.buttonStates) {
       setButtonStates(location.state.buttonStates);
     }
   }, [location.state]);
 
-  /*const toggleIcon = index => {
-    const newButtonStates = [...buttonStates];
-    newButtonStates[index] = !newButtonStates[index];
-    setButtonStates(newButtonStates);
-  };*/
-
   const back = () => {
     navigate('/', { state: { buttonStates: buttonStates } });
   };
+
+  // buttonStatesとcorrectAnswersが一致するかどうかをチェック
+  const isMatching = JSON.stringify(buttonStates) === JSON.stringify(correctAnswers);
 
   return (
     <div className="desktop">
       <div className="frame">
         {buttonStates.map((isVisible, index) => (
           <div className="box" key={index}>
-            <button className="rectangle-button" /*onClick={() => toggleIcon(index)}*/>
+            <button className="rectangle-button">
               <div className="rectangle">
                 {isVisible && <img className="icon-check" alt="Icon check" src={icon} />}
               </div>
             </button>
-            <div className="ling" />
+            <div className="ling" 
+            style={{border: correctAnswers[index] ? '1.5px solid var(--Gradient-8, #2FB8FF)' : 'none'}} 
+            />
           </div>
         ))}
       </div>
       <div className="title">
-        <div className="model-s">あなたはAIをだませた！</div>
+        <div className="model-s">{isMatching ? 'あなたはAIをだませた！' : 'あなたはAIをだませなかった！'}</div>
       </div>
       <button className="input-field-button" onClick={back}>
         <div className="input-field">
