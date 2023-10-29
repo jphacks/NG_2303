240089c6d9a3fc93948e59474ae82acc1aefba64
neruneruna7 <113534341+neruneruna7@users.noreply.github.com>
2023-10-29 14:51:03 +0900

    fix

--- data_accumulation/src/gcp.rs
+++ data_accumulation/src/gcp.rs
@@ -2,7 +2,7 @@ use serde_json::json;
 
 // visionAPIにリクエストを投げて，物体検出する
 pub async fn ocject_detect(api_key: &str, image_path:&str ) -> anyhow::Result<()>{
-    let access_url = format!("POST https://vision.googleapis.com/v1/images:annotate?key={}", api_key);
+    let access_url = format!("https://vision.googleapis.com/v1/images:annotate?key={}", api_key);
 
     let body = json!({
         "requests": [
@@ -24,6 +24,7 @@ pub async fn ocject_detect(api_key: &str, image_path:&str ) -> anyhow::Result<()
 
     let client = reqwest::Client::new();
     let res = client.post(&access_url)
+        .header("Content-Type", "application/json")
         .json(&body)
         .send()
         .await?;
