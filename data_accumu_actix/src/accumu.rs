7a238a26fdb610d8e740320832e0dfdd9f19028c
neruneruna7 <113534341+neruneruna7@users.noreply.github.com>
2023-10-29 09:09:51 +0900

    update: 1day終了時点

--- data_accumu_actix/src/accumu.rs
+++ data_accumu_actix/src/accumu.rs
@@ -3,78 +3,80 @@ use serde::{Deserialize, Serialize};
 use sqlx::{Database, SqlitePool};
 use async_trait::async_trait;
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct FrontendData {
-    image_label: String,
-    image_base64_vec: Vec<ImageData>,
-}
 
-impl FrontendData {
-    pub fn new(image_label: String, image_base64_vec: Vec<ImageData>) -> Self {
+/// フロント，バック間で送信，受信されるデータ型
+#[derive(Serialize, Deserialize, Debug)]
+struct  BeJudgeImages {
+    object_label: String, //be_judge_imagesに含まれる画像のラベル 1種類しかないはずなので，ここにも持ってきた
+    noized_images: Vec<NoizedImage>,
+}
+impl BeJudgeImages {
+    pub fn new(object_label: String, noized_images: Vec<NoizedImage>) -> Self { Self { object_label, noized_images } }
+
+    pub fn new_dammy() -> Self {
         Self {
-            image_label,
-            image_base64_vec,
-        }
-    }
-    pub fn new2() -> Self {
-        Self {
-            image_label: "".to_string(),
-            image_base64_vec: vec![],
+            object_label: "".to_string(),
+            noized_images: vec![NoizedImage::new_dammy()],
         }
     }
 }
 
+/// 画像1つあたりのデータ型
+/// DBにもこの型で保存する
 #[derive(Serialize, Deserialize, Debug)]
-pub struct ImageData {
-    image_base64: String,
-    // 複数の物体が含まれている場合は，複数のラベルを持つ．
-    label: Vec<String>,
-    forbidden_label: bool,
-    noize_info: String,
+pub struct NoizedImage {
+    image_url: String, // Amazon S3からのURL
+    //image_base64: String,になる可能性もある
+    object_label: String,
+    noize_info: String, //どのような構造の情報が入るのか決定できないためString
+    forbidden_label: bool,   // ユーザが選択した場合，botの可能性を疑うことを示すラベル
 }
+
+
+impl NoizedImage {
+    pub fn new(image_url: String, object_label: String, noize_info: String, forbidden_label: bool) -> Self { Self { image_url, object_label, noize_info, forbidden_label } }
+
+    pub fn new_dammy() -> Self {
+        Self {
+            image_url: "".to_string(),
+            object_label: "".to_string(),
+            noize_info: "".to_string(),
+            forbidden_label: false,
+        }
+    }
+}
+
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ObjectDetectionData {
-    pub image_base64: String,    // 画像データ
-    pub true_label: String,      // 物体の本来のラベル
+    pub image_url: String,    // Amazon S3からのURL
+    pub object_label: String,      // 物体の本来のラベル
     pub predicted_label: String, // 物体の誤認識の結果のラベル
+    pub confidence: f64,         // 物体が存在する確信度
     pub forbidden_label: bool,   // ユーザが選択してはいけないことを示すラベル（2値）
     pub noise_info: String,      // どのようなノイズがかかっているかの情報
-                                 // objects: Vec<Object>,    // 物体検出結果のオブジェクトのリスト
 }
 
 impl ObjectDetectionData {
     pub fn new(
-        image_base64: String,
-        true_label: String,
+        image_url: String,
+        object_label: String,
         predicted_label: String,
+        confidence: f64,
         forbidden_label: bool,
         noise_info: String,
     ) -> Self {
         Self {
-            image_base64,
-            true_label,
+            image_url,
+            object_label,
             predicted_label,
+            confidence,
             forbidden_label,
             noise_info,
         }
     }
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Object {
-    label: String,     // 物体のラベル
-    confidence: f32,   // 物体が存在する確信度
-    bbox: BoundingBox, // 物体のバウンディングボックス
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct BoundingBox {
-    x_min: f32, // バウンディングボックスの左端のx座標
-    y_min: f32, // バウンディングボックスの上端のy座標
-    x_max: f32, // バウンディングボックスの右端のx座標
-    y_max: f32, // バウンディングボックスの下端のy座標
-}
 
 /// DIのためのtrait
 #[async_trait]
@@ -83,3 +85,19 @@ pub(crate) trait DataAccumu {
     async fn insert(&self, data: ObjectDetectionData, pool: SqlitePool) -> Result<()>;
     async fn delete(&self, id: i64, pool: SqlitePool) -> Result<()>;
 }
+
+// 少なくともこの2日の期間中は廃止するデータ型
+// #[derive(Serialize, Deserialize, Debug)]
+// pub struct Object {
+//     label: String,     // 物体のラベル
+//     confidence: f32,   // 物体が存在する確信度
+//     bbox: BoundingBox, // 物体のバウンディングボックス
+// }
+
+// #[derive(Serialize, Deserialize, Debug)]
+// pub struct BoundingBox {
+//     x_min: f32, // バウンディングボックスの左端のx座標
+//     y_min: f32, // バウンディングボックスの上端のy座標
+//     x_max: f32, // バウンディングボックスの右端のx座標
+//     y_max: f32, // バウンディングボックスの下端のy座標
+// }
--- migrations/20231028000000_database.sql
+++ migrations/20231028000000_database.sql
@@ -1,13 +1,19 @@
 CREATE TABLE IF NOT EXISTS object_detection_data (
     id     INTEGER    PRIMARY KEY     NOT NULL,
-    label    TEXT          NOT NULL,
-    image_recognition_result     TEXT                NOT NULL,
-    base64_image     TEXT                NOT NULL
+    image_url    TEXT          NOT NULL,
+    object_label    TEXT          NOT NULL,
+    predicted_label    TEXT          NOT NULL,
+    confidence    REAL          NOT NULL,
+    forbidden_label    INTEGER          NOT NULL,
+    -- 本来はboor型にしたいが，sqlite3ではboolean型がないためinteger型で代用
+    noize_info     TEXT                NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS noized_images (
     id     INTEGER    PRIMARY KEY     NOT NULL,
     image_url    TEXT          NOT NULL,
+    object_label    TEXT          NOT NULL,
     noize_info     TEXT                NOT NULL,
-    object_label    TEXT                NOT NULL
+    forbidden_label    INTEGER                NOT NULL
+    -- 本来はboor型にしたいが，sqlite3ではboolean型がないためinteger型で代用
 );
